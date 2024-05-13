8d0d998c0375ad753c16e4eb01555a1f9f9c1dbc
baberabb <92168766+baberabb@users.noreply.github.com>
2024-05-13 10:51:30 +0500

    chore($eslint): Update eslint and package.json files

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
+    'prettier',
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   rules: {},
